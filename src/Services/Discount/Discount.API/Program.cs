13e1313d155cfba4da3aec4f754d47a6c0f681a8
MarcosFerreira17 <marcosfw7@outlook.com>
2022-08-04 21:22:09 -0300

    update db

--- src/Services/Discount/Discount.API/Program.cs
+++ src/Services/Discount/Discount.API/Program.cs
@@ -13,7 +13,9 @@ namespace Discount.API
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            host.MigrateDatabase<Program>();
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
--- src/Services/Discount/Discount.API/Startup.cs
+++ src/Services/Discount/Discount.API/Startup.cs
@@ -1,3 +1,4 @@
+using Discount.API.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -27,6 +28,7 @@ namespace Discount.API
         {
 
             services.AddControllers();
+            services.AddScoped<IDiscountRepository, DiscountRepository>();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Discount.API", Version = "v1" });
