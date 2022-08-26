285d4c1c784cc4202232b5b76b88c880b1eadb38
MarcosFerreira17 <marcosfw7@outlook.com>
2022-08-25 22:52:08 -0300

    Orders Querys

---
+++ src/Services/Ordering/Ordering.Application/Contracts/Infraestructure/IEmailService.cs
@@ -0,0 +1,10 @@
+﻿using Ordering.Application.Models;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Contracts.Infraestructure
+{
+    public interface IEmailService
+    {
+        Task<bool> SendEmail(Email email);
+    }
+}
